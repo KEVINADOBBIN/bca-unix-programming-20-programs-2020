734da4d9e14555207e176198fb93c2b4bea5af2e
Kevin A Dobbin <94518887+KEVINADOBBIN@users.noreply.github.com>
2026-01-04 13:26:11 +0530

    Add files via upload

---
+++ palindrome.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+echo "Enter a number:"
+read num
+
+n=$num
+rev=0
+
+while [ $num -gt 0 ]
+do
+    rem=$((num % 10))
+    rev=$((rev * 10 + rem))
+    num=$((num / 10))
+done
+
+echo "Original number is $n"
+echo "Reverse number is $rev"
+
+if [ $n -eq $rev ]
+then
+    echo "$n is a palindrome"
+else
+    echo "$n is not a palindrome"
+fi
